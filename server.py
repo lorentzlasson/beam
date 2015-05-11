93c873a1b84734ac672bb4e714ed8193d86b07f6
Simon Lagos <lagos@Simon-Lagoss-MacBook-Pro-2.local>
2015-05-11 18:04:39 +0200

    Database connection

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import os
+import psycopg2 as psycopg2
+import simplejson as json
+
 try:
   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
   from SocketServer import TCPServer as Server
@@ -11,11 +14,43 @@ PORT = int(os.getenv('VCAP_APP_PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
 
+
 httpd = Server(("", PORT), Handler)
+
+
+def connectToDb():
+    db_url = ""
+    if 'VCAP_SERVICES' in os.environ:
+        data = json.loads(os.environ['VCAP_SERVICES'])
+        db_url = data["elephantsql"][0]["credentials"]["uri"]
+        print (db_url)
+    if db_url is "":
+        with open('VCAP_SERVICES.json') as data_file:
+            data = json.load(data_file)
+            db_url = data["VCAP_SERVICES"]["elephantsql"][0]["credentials"]["uri"]
+            print (db_url)
+    try:
+        conn = psycopg2.connect(db_url)
+        print "connected"
+    except:
+        print "I am unable to connect to the database"
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * from \"user\"")
+        rows = cur.fetchall()
+        print "\nShow me the databases:\n"
+        for row in rows:
+            print "   ", row[0]
+    except:
+        print "Can't display result"
+
+
 try:
   print("Start serving at port %i" % PORT)
+  connectToDb()
   httpd.serve_forever()
 except KeyboardInterrupt:
   pass
 httpd.server_close()
+
 
